87d02647a6aee70bace860de63f6f20aafb9f08f
Khushal <khushal2004a@gmail.com>
2025-10-24 13:24:20 +0530

    feat: vercel.json

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
